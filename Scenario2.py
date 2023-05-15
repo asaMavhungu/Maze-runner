c38d84f7e099a282d9941536f5edcbd080a92328
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-05-16 01:22:07 +0200

    Added path tracing to terminal output

--- Scenario2.py
+++ Scenario2.py
@@ -6,12 +6,17 @@ import utils
 from time import sleep
 import os
 
+
 def main():
+
+	aTypes = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+	gTypes = ['EMPTY', 'RED', 'GREEN', 'BLUE']
+
 	# Environment
 	env = FourRooms('multi')
 
 	# Agent
-	agent = RLAgent1(num_states=13*13*3,
+	agent = RLAgent1(num_states=13*13*8,
 					num_actions=4,
 					alpha=0.1,
 					gamma=0.99,
@@ -32,8 +37,11 @@ def main():
 		cpy[currY][currX] = 5
 
 		packages = []
-		binary2 = "000"
-		binary10 = "000"
+		binary2 = [0,0,0]
+
+		origin = env.getPosition()
+
+		actPosGrid = []
 
 		while not done:
 			action = agent.choose_action(state)
@@ -46,46 +54,53 @@ def main():
 	
 			gridType, newPos, packagesRemaining, isTerminal = env.takeAction(action)
 
-			#currX, currY = env._FourRooms__current_pos # type: ignore[attr]
-			#cpy[currY][currX] = 5
-
-
-			#os.system('clear')
-			#utils.print_colored_maze(cpy)
-
-			#sleep(.05)
 			
+		
 			next_state = (13*newPos[0] + newPos[1])
 			if gridType > 0 and gridType not in packages:
 				
 				packages.append(gridType)
 				reward = 100*(4-len(packages))
 
+				binary2[gridType-1] = 1
+
 			elif cpy[currY][currX] == 4:
-				reward = -20
+				reward = -20 # Punnish backtracking
 			else:
 				reward = -8
 
-			done = isTerminal
+			
+			offset = binary2[0]* 2**2 + binary2[1]* 2**1 + binary2[2]* 2**0
+			
+			next_state = offset*(13*13) + (13*newPos[0] + newPos[1])
 
-			#next_state, reward, done, _ = env.step(action)
+			done = isTerminal
 
 			agent.learn(state, action, reward, next_state, done)
 			state = next_state
 			total_reward += reward
+
+
+			actPosGrid.append((action, newPos, gridType))
+
 		rewards[i] = total_reward
 		os.system('clear')
 		utils.print_colored_maze(cpy)
 		print(f"Episode: {i}")
 		print(f"Packs: {packages}")
 		print(f"Total reward:{total_reward}")
+
+		print('Agent starts at: {0}'.format(origin))
+
+		for ten in actPosGrid:
+			print("Agent took {0} action and moved to {1} of type {2}".format (aTypes[ten[0]], ten[1], gTypes[ten[2]]))
 		sleep(.05)
 
 	# Plot Results
-	#plt.plot(rewards)
-	#plt.xlabel('Episode')
-	#plt.ylabel('Reward')
-	#plt.show()
+	plt.plot(rewards)
+	plt.xlabel('Episode')
+	plt.ylabel('Reward')
+	plt.show()
 
 	# Show Path
 	#env.showPath(-1)
