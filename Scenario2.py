4eabf57fc9d7a9c9fcf5ea772906fae6c68b534a
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-05-16 00:23:43 +0200

    Copied scenerio 1 as a layout

---
+++ Scenario2.py
@@ -0,0 +1,94 @@
+from FourRooms import FourRooms
+import matplotlib.pyplot as plt
+import numpy as np
+from RLAgent import RLAgent1
+import utils
+from time import sleep
+import os
+
+def main():
+	# Environment
+	env = FourRooms('multi')
+
+	# Agent
+	agent = RLAgent1(num_states=13*13*3,
+					num_actions=4,
+					alpha=0.1,
+					gamma=0.99,
+					epsilon=0.05)
+
+	# Training
+	num_episodes = 1000
+	rewards = np.zeros(num_episodes)
+	for i in range(num_episodes):
+		env.newEpoch()
+		cpy = env._FourRooms__environment.copy() # type: ignore[attr]
+		x, y = env.getPosition()
+		state = (13*x+y)
+		done = False
+		total_reward = 0
+		
+		currX, currY = env._FourRooms__current_pos # type: ignore[attr]
+		cpy[currY][currX] = 5
+
+		packages = []
+		binary2 = "000"
+		binary10 = "000"
+
+		while not done:
+			action = agent.choose_action(state)
+
+			currX, currY = env._FourRooms__current_pos # type: ignore[attr]
+
+			val = cpy[currY][currX]
+			if val == 0:
+				cpy[currY][currX] = 4
+	
+			gridType, newPos, packagesRemaining, isTerminal = env.takeAction(action)
+
+			#currX, currY = env._FourRooms__current_pos # type: ignore[attr]
+			#cpy[currY][currX] = 5
+
+
+			#os.system('clear')
+			#utils.print_colored_maze(cpy)
+
+			#sleep(.05)
+			
+			next_state = (13*newPos[0] + newPos[1])
+			if gridType > 0 and gridType not in packages:
+				
+				packages.append(gridType)
+				reward = 100*(4-len(packages))
+
+			elif cpy[currY][currX] == 4:
+				reward = -20
+			else:
+				reward = -8
+
+			done = isTerminal
+
+			#next_state, reward, done, _ = env.step(action)
+
+			agent.learn(state, action, reward, next_state, done)
+			state = next_state
+			total_reward += reward
+		rewards[i] = total_reward
+		os.system('clear')
+		utils.print_colored_maze(cpy)
+		print(f"Episode: {i}")
+		print(f"Packs: {packages}")
+		print(f"Total reward:{total_reward}")
+		sleep(.05)
+
+	# Plot Results
+	#plt.plot(rewards)
+	#plt.xlabel('Episode')
+	#plt.ylabel('Reward')
+	#plt.show()
+
+	# Show Path
+	#env.showPath(-1)
+
+if __name__ == "__main__":
+    main()
